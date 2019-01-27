d77061045e2684984bcdc84fb366619e9f9866a4
tarun <gemini86.tarun@gmail.com>
2019-01-27 20:52:42 +0530

    Deletion

--- src/main/java/com/app/MavenSpringBootMvcAopRestApiOnlineShoppingWithAngular6AndMongodb/modal/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.app.MavenSpringBootMvcAopRestApiOnlineShoppingWithAngular6AndMongodb.modal;
-
-public enum OrderStatus {
-    PAID
-}
--- src/main/java/com/app/MavenSpringBootMvcAopRestApiOnlineShoppingWithAngular6AndMongodb/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.MavenSpringBootMvcAopRestApiOnlineShoppingWithAngular6AndMongodb.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.app.MavenSpringBootMvcAopRestApiOnlineShoppingWithAngular6AndMongodb.modal.Order;
-
-@Repository
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
